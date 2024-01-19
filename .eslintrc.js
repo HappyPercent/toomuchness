e9047b4d9546dc5710a649ea66c0d7822df6d388
andrey-erofteev <erofteev@gmail.com>
2024-01-19 14:04:05 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['prettier', '@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        project: './tsconfig.base.json',
+    },
+    rules: {},
+};
